87694048ac66946d9c744a072074f3d49b54df3c
urfin78 <urfin78@gmail.com>
2019-09-19 23:34:32 +0200

    readme and code

---
+++ travis/after_success.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "${DOCKER_PW}" | docker login -u "${DOCKER_USER}" --password-stdin
+docker push ${DOCKER_USER}/prometheus:${VERSION}-${ARCH}
+docker manifest create ${DOCKER_USER}/prometheus:${VERSION} ${DOCKER_USER}/prometheus:${VERSION}-i386 ${DOCKER_USER}/prometheus:${VERSION}-arm32v6 ${DOCKER_USER}/prometheus:${VERSION}-amd64
+docker manifest annotate --arch arm --os linux --variant armv6l ${DOCKER_USER}/prometheus:${VERSION} ${DOCKER_USER}/prometheus:${VERSION}-arm32v6
+docker manifest annotate --arch 386 --os linux ${DOCKER_USER}/prometheus:${VERSION} ${DOCKER_USER}/prometheus:${VERSION}-i386
+docker manifest annotate --arch amd64 --os linux ${DOCKER_USER}/prometheus:${VERSION} ${DOCKER_USER}/prometheus:${VERSION}-amd64
+docker manifest push ${DOCKER_USER}/prometheus:${VERSION}
---
+++ travis/script.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce
+
+if [ ${ARCH} == "amd64" ];then
+  export DOCKERFILE="Dockerfile.amd64"
+else
+  export DOCKERFILE="Dockerfile"
+  QEMU_USER_STATIC_DOWNLOAD_URL="https://github.com/multiarch/qemu-user-static/releases/download"
+  QEMU_USER_STATIC_LATEST_TAG=$(curl -s https://api.github.com/repos/multiarch/qemu-user-static/tags|grep 'name.*v[0-9]'| head -n 1| cut -d '"' -f 4)
+  curl -SL "${QEMU_USER_STATIC_DOWNLOAD_URL}/${QEMU_USER_STATIC_LATEST_TAG}/x86_64_qemu-${QEMU_ARCH}-static.tar.gz"|tar xzv
+  docker run --rm --privileged multiarch/qemu-user-static:register --reset
+fi
+
+if [ ${VERSION} == "latest" ]; then
+  export SW_VERSION=master
+else
+  export SW_VERSION=${VERSION}
+fi
+docker build --build-arg GOLANG_ARCH=${GOARCH} --build-arg SW_VERSION=${SW_VERSION} --build-arg ARCH=${ARCH} --build-arg QEMU_ARCH=${QEMU_ARCH} -t urfin78/prometheus:${VERSION}-${ARCH} -f ${DOCKERFILE} .
