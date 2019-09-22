5b08a7f9569e3a2f89c936eba8830b1869f88c74
urfin78 <urfin78@gmail.com>
2019-09-22 19:23:55 +0200

    rebstructure dockerfiles and arch

--- travis/script.sh
+++ travis/script.sh
@@ -6,10 +6,9 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update
 sudo apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce
 
-if [ ${ARCH} == "amd64" ];then
-  export DOCKERFILE="Dockerfile.amd64"
-else
-  export DOCKERFILE="Dockerfile"
+export DOCKERFILE="Dockerfile.${ARCH}"
+
+if [ ${ARCH} != "amd64" ];then
   QEMU_USER_STATIC_DOWNLOAD_URL="https://github.com/multiarch/qemu-user-static/releases/download"
   QEMU_USER_STATIC_LATEST_TAG=$(curl -s https://api.github.com/repos/multiarch/qemu-user-static/tags|grep 'name.*v[0-9]'| head -n 1| cut -d '"' -f 4)
   curl -SL "${QEMU_USER_STATIC_DOWNLOAD_URL}/${QEMU_USER_STATIC_LATEST_TAG}/x86_64_qemu-${QEMU_ARCH}-static.tar.gz"|tar xzv
@@ -21,9 +20,5 @@ if [ ${VERSION} == "latest" ]; then
 else
   export SW_VERSION=${VERSION}
 fi
-if [ ${ARCH} == "arm32v6" ]; then
-  export DISTRO=alpine
-else
-  export DISTRO=buster
-fi
-docker build --build-arg DISTRO=${DISTRO} --build-arg GOLANG_ARCH=${GOARCH} --build-arg SW_VERSION=${SW_VERSION} --build-arg ARCH=${ARCH} --build-arg QEMU_ARCH=${QEMU_ARCH} -t urfin78/prometheus:${VERSION}-${ARCH} -f ${DOCKERFILE} .
+
+docker build --build-arg GOLANG_ARCH=${GOARCH} --build-arg SW_VERSION=${SW_VERSION} --build-arg ARCH=${ARCH} --build-arg QEMU_ARCH=${QEMU_ARCH} -t urfin78/prometheus:${VERSION}-${ARCH} -f ${DOCKERFILE} .
